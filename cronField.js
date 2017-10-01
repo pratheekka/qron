45ee2164ce6ed49a1a342e02da40e9e02e7ab6d8
Pratheek K A <pratheek.kap@gmail.com>
2017-10-01 08:40:12 +0530

    Add initial regex validation

--- cronField.js
+++ cronField.js
@@ -1,19 +1,46 @@
-export const FIELDS = {
-	SECONDS: {},
-	MINUTES: {},
-	HOURS: {},
-	DAYS: {},
-	MONTHS: {},
-	WEEKDAYS: {},
-	YEAR: {}
+const FIELDPROPS = {
+	s: {
+		regex: new RegExp('.+')
+	},
+	m: {
+		regex: new RegExp('.+')
+	},
+	h: {
+		regex: new RegExp('.+')
+	},
+	d: {
+		regex: new RegExp('.+')
+	},
+	M: {
+		regex: new RegExp('.+')
+	},
+	w: {
+		regex: new RegExp('.+')
+	},
+	y: {
+		regex: new RegExp('.+')
+	}
+},
+validate = (field, value) => {
+	const fieldProp = FIELDPROPS[field];
+
+	return fieldProp.regex.test(value);
+
+};
+
+export const FIELDS_ENUM = {
+	SECONDS: "s",
+	MINUTES: "m",
+	HOURS: "h",
+	DAYS: "d",
+	MONTHS: "M",
+	WEEKDAYS: "w",
+	YEAR: "y"
 };
 
 export default class cronField {
 	constructor(field, fieldValue) {
-		
-	}
-
-	get isValid() {
-		
+		this.value = fieldValue;
+		this.isValid = validate(field, fieldValue);
 	}
 }
